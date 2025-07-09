27bb74e93d27a462e0de5d947f1e3252d7321bde
n1nme18290 <cutealisa1010@gmail.com>
2025-07-09 21:22:04 +0800

    Angular-Page

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ComponentsComponent } from './components/components.component';
+
+export const routes: Routes = [
+    { path: 'components', component: ComponentsComponent }
+];
---
+++ src/app/components/components.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { FormsModule } from '@angular/forms';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzDrawerModule} from 'ng-zorro-antd/drawer';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+
+
+
+@Component({
+  selector: 'app-components',
+  standalone: true,
+  imports: [NzButtonModule, NzIconModule, NzInputModule, NzTypographyModule, NzDropDownModule, FormsModule
+    , NzSelectModule, NzSwitchModule, NzAvatarModule, NzTabsModule, NzPageHeaderModule,NzDrawerModule,
+    NzRadioModule,
+
+  ],
+  templateUrl: './components.component.html',
+  styleUrl: './components.component.scss'
+})
+export class ComponentsComponent {
+
+  passwordVisible = false;
+  password?: string;
+
+  selectedValue = null;
+
+  onBack(): void {
+    console.log('onBack');
+  }
+  visible = false;
+
+  open(): void {
+    this.visible = true;
+  }
+
+  close(): void {
+    this.visible = false;
+  }
+
+
+}
