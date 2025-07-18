a9029fd6040ff777ef19e58dabd170041dbb6238
n1nme18290 <cutealisa1010@gmail.com>
2025-07-18 20:27:58 +0800

    add new page

--- src/app/core/app.routes.ts
+++ src/app/core/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
+import { DemoComponentsComponent } from '../share/demo-components/demo-components.component';
 import { LogInComponent } from '../feature/log-in/log-in.component';
 import { RegisterComponent } from '../feature/register/register.component';
 import { PersonalInfoComponent } from '../feature/personal-info/personal-info.component';
-import { DemoComponentsComponent } from '../share/demo-components/demo-components.component';
+import { EquipmentComponent } from '../feature/equipment/equipment.component';
+import { EquipmentInformationComponent } from '../feature/equipment-information/equipment-information.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+    { path: "demo-components", component:DemoComponentsComponent},
     { path: 'log-in', component: LogInComponent },
     { path: 'register', component:RegisterComponent},
     { path: "personal-info", component:PersonalInfoComponent},
-    { path: "demo-components", component:DemoComponentsComponent},
+    { path: "equipment", component:EquipmentComponent},
+    { path: "equipment-information", component:EquipmentInformationComponent},
 ];
